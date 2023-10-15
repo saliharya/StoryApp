0c9710aabab5ba66489564ccfffe1bcfad03aab5
saliharya <saliharyazinc@gmail.com>
2023-10-15 14:52:29 +0700

    change targetsdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     defaultConfig {
         applicationId = "com.arya.storyapp"
         minSdk = 27
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,6 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.44")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.48.1")
     }
 }

8849b97dbf0dff289044dccb774db2e2b42ca647
saliharya <saliharyazinc@gmail.com>
2023-11-05 03:14:40 +0700

    implement shared element animation

--- app/src/main/java/com/arya/storyapp/ui/adapter/ListStoryAdapter.kt
+++ app/src/main/java/com/arya/storyapp/ui/adapter/ListStoryAdapter.kt
@@ -1,11 +1,14 @@
 package com.arya.storyapp.ui.adapter
 
+import android.app.Activity
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.util.Pair
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.arya.storyapp.R
@@ -27,14 +30,23 @@ class ListStoryAdapter :
         holder.itemView.setOnClickListener {
             val intent = Intent(holder.itemView.context, StoryDetailActivity::class.java)
             intent.putExtra("story", story)
-            holder.itemView.context.startActivity(intent)
+
+            val optionsCompat: ActivityOptionsCompat =
+                ActivityOptionsCompat.makeSceneTransitionAnimation(
+                    holder.itemView.context as Activity,
+                    Pair(holder.storyImageView, "image"),
+                    Pair(holder.usernameTextView, "username"),
+                    Pair(holder.descriptionTextView, "description")
+                )
+
+            holder.itemView.context.startActivity(intent, optionsCompat.toBundle())
         }
     }
 
     class StoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val storyImageView: ImageView = itemView.findViewById(R.id.ivStory)
-        private val usernameTextView: TextView = itemView.findViewById(R.id.tvUsername)
-        private val descriptionTextView: TextView = itemView.findViewById(R.id.tvDescription)
+        internal val storyImageView: ImageView = itemView.findViewById(R.id.ivStory)
+        internal val usernameTextView: TextView = itemView.findViewById(R.id.tvUsername)
+        internal val descriptionTextView: TextView = itemView.findViewById(R.id.tvDescription)
 
         fun bind(story: Story) {
             with(story) {

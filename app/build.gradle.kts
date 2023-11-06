34b45d7cf60bafb3a61403f640024107a6e6e7b5
saliharya <saliharyazinc@gmail.com>
2023-11-06 22:37:27 +0700

    add paging dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
-    implementation("com.google.android.gms:play-services-maps:18.0.2")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("com.google.android.gms:play-services-location:21.0.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -67,6 +67,7 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.8.0")
     implementation("androidx.exifinterface:exifinterface:1.3.6")
     kapt("com.google.dagger:hilt-android-compiler:2.48.1")
+    implementation("androidx.paging:paging-runtime:3.2.1")
 }
 
 kapt {
